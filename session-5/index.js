53e8c457bd755064914ee014f93d138e2ac56894
CamilleM28 <Camillem2812@gmail.com>
2021-12-06 21:41:32 +0100

    Task 2

--- session-5/index.js
+++ session-5/index.js
@@ -1,19 +1,48 @@
 function getGrade(total) {
-  if (total >= 90) {
+  if (total > 90) {
     return "A+";
-  } else if (total >= 80 && total < 90) {
+  } else if (total > 80 && total <= 90) {
     return "A";
-  } else if (total >= 70 && total < 80) {
+  } else if (total > 70 && total <= 80) {
     return "B";
-  } else if (total >= 60 && total < 70) {
+  } else if (total > 60 && total <= 70) {
     return "C";
-  } else if (total >= 50 && total < 60) {
+  } else if (total > 50 && total <= 60) {
     return "D";
-  } else if (total >= 40 && total < 50) {
+  } else if (total > 40 && total <= 50) {
     return "E";
-  } else if (total >= 30 && total < 40) {
+  } else if (total > 30 && total <= 40) {
     return "F";
   } else return "Fail";
 }
 
-console.log(getGrade(44));
+console.log(getGrade(67));
+
+function getGrade(total) {
+  switch (true) {
+    case total > 90:
+      return "A+";
+      break;
+    case total > 80 && total <= 90:
+      return "A";
+      break;
+    case total > 70 && total <= 80:
+      return "B";
+    case total > 60 && total <= 70:
+      return "C";
+      break;
+    case total > 50 && total <= 60:
+      return "D";
+      break;
+    case total > 40 && total <= 50:
+      return "E";
+      break;
+    case total > 30 && total <= 40:
+      return "F";
+      break;
+    default:
+      return "Fail";
+  }
+}
+
+console.log(getGrade(20));
