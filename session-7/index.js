9c4ab7b8c975f8165e092d2ffdb891d2f3bf56d7
CamilleM28 <Camillem2812@gmail.com>
2021-12-15 21:52:22 +0000

    session 7 tasks

--- session-7/index.js
+++ session-7/index.js
@@ -1,38 +1,31 @@
 // ****************************************************TASK 1****************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const animals = ["cat", "dog", "wolf", "lion", "eagle", "Zebra"];
 
 // ****************************************************TASK 2****************************************************
+const names = [
+  "camille",
+  "sam",
+  "maya",
+  "hanan",
+  "jess",
+  "ben",
+  "george",
+  "lewis",
+  "sarah",
+  "jane",
+];
 
+const numbers = [5, 325, 67, 100000, 150];
 
-
-
-
-
-
-
-
-
+console.log(numbers.sort((a, b) => a - b));
 
 // ****************************************************TASK 3****************************************************
 
 // Notice the backticks enclosing t'challa. Read https://www.javascripttutorial.net/es6/javascript-template-literals/ for an explanation on template literals
 const people = [
   { name: `t'challa`, age: 37 },
-  { name: 'shuri', age: 16 },
-  { name: 'killmonger', age: 32 },
+  { name: "shuri", age: 16 },
+  { name: "killmonger", age: 32 },
 ];
 
 // This is a helper function that should be used in the final task of the session.
@@ -44,3 +37,11 @@ function compare(a, b) {
 
   return personA > personB ? true : false;
 }
+
+const inOrder = people.sort(compare).map((people) => people.name);
+
+// function inOrder() {
+//   return people.sort(compare).map((people) => people.name);
+// }
+
+// console.log(inOrder());
